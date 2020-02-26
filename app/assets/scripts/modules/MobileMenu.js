3f8628eb1fa9775e0d97a9356cc9050cb8ce03fd
Max <max@example.com>
2020-02-26 12:26:38 -0800

    GIT_WEB Lesson 044 Adjusting Our Menu

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,21 +2,19 @@ import $ from 'jquery';
 
 class MobileMenu {
 	constructor() {
+		this.siteHeader = $(".site-header");
 		this.menuIcon = $(".site-header__menu-icon");
 		this.menuContent = $(".site-header__menu-content");
 		this.events();
 	}
 	
 	events() {
-		console.log(this);
 		this.menuIcon.click( this.toggleTheMenu.bind(this) );
-		console.log(this);
 	}
 	
 	toggleTheMenu() {
-		console.log(this);
 		this.menuContent.toggleClass("site-header__menu-content--is-visible");
-		console.log(this);
+		this.siteHeader.toggleClass("site-header--is-expanded");
 	}
 }
 
