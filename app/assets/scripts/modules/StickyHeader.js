678267f6500602b9d101ee3766cf881ad21d6974
Max <max@example.com>
2020-02-29 13:27:47 -0800

    GIT_WEB Lesson 049 Desktop Sticky Header (Part 2)

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,14 +1,25 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
 	constructor() {
 		this.siteHeader = $(".site-header");
 		this.largeHeroTitle  = $(".large-hero__title");
-		this.createWaypoints();
+		this.createHeaderWaypoint();
+		
+		this.headerLinks  = $(".primary-nav a");
+		this.pageSections = $(".page-section");
+		this.createPageSectionWaypoints();
+		
+		this.addSmoothScroll();
 	}
 	
-	createWaypoints() {
+	addSmoothScroll() {
+		this.headerLinks.smoothScroll();
+	}
+	
+	createHeaderWaypoint() {
 		var triggerPoint    = this.largeHeroTitle[0];
 		var objStickyHeader = this.siteHeader;
 		new Waypoint({
@@ -21,6 +32,35 @@ class StickyHeader {
 				}
 			}
 		});
+	}
+	
+	createPageSectionWaypoints() {
+		var objHeaderLinks = this.headerLinks;
+		this.pageSections.each( function() {
+			var currentPageSection = this;
+			
+			new Waypoint({
+				element: currentPageSection, 
+				handler: function(direction) {
+					if ( direction == "down" ) {
+						objHeaderLinks.removeClass("is-current-link");
+						$(currentPageSection.getAttribute("data-matching-link")).addClass("is-current-link");
+					}
+				}, 
+				offset: "18%"
+			});
+			
+			new Waypoint({
+				element: currentPageSection, 
+				handler: function(direction) {
+					if ( direction == "up" ) {
+						objHeaderLinks.removeClass("is-current-link");
+						$(currentPageSection.getAttribute("data-matching-link")).addClass("is-current-link");
+					}
+				}, 
+				offset: "-40%"
+			});
+		} );
 	}
 }
 
